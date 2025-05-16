40591ffee1ee2a22308c2983218d9e2216bff828
Iam-Ujin <dklvmf0106@naver.com>
2025-05-16 16:07:27 +0900

    📝 250516 react 수업

---
+++ react/0516/my-app2/src/data/profileData.js
@@ -0,0 +1,26 @@
+export const profileData = [
+    {
+        id: 1,
+        image: "https://i.namu.wiki/i/EAQmQYRmUJN_VoVwUw_lJQEFWPwK2mcoGwUGe8r7tdDO6koiG1IFGou6e04lCxdLJ68BDBVOW_dYmTbnPtfPrA.webp",
+        name: "지지",
+        job: "프론트엔드 개발자",
+        isWorking: false,
+        company: null,
+    },
+    {
+        id: 2,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRahFteSXpjYzUTqdtzQ5dR-1xhGstnkARA3A&s",
+        name: "하티",
+        job: "프론트엔드 개발자",
+        isWorking: true,
+        company: "위니브",
+    },
+    {
+        id: 3,
+        image: "https://previews.123rf.com/images/mazikab/mazikab1312/mazikab131200012/26053008-%EA%B2%80%EC%9D%80-%ED%86%A0%EB%81%BC%EB%8A%94-%EC%A0%95%EC%9B%90%EC%97%90%EC%84%9C-%EB%A8%B9%EC%9D%B4%EC%9E%85%EB%8B%88%EB%8B%A4.jpg",
+        name: "로지",
+        job: "프론트엔드 개발자",
+        isWorking: true,
+        company: "위니브",
+    },
+];
