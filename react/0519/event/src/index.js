788000727f45078fcd1aae155fb0ef75e58672a3
Iam-Ujin <dklvmf0106@naver.com>
2025-05-20 16:15:54 +0900

    📝 250519 react 파일명 수정

--- react/0519/event/src/index.js
+++ react/0519/event/src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import App2 from "./App2";
 import App3 from "./App3";
 import App4 from "./App4";
-import App5 from "./\bApp5";
+import App5 from "./App5";
 import TabNavigation from "./components/TabNavigation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
