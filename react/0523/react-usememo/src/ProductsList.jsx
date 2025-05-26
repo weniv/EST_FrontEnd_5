5f54acce95b66b7359ad7db855d5df4f751b2cdb
nugurejeil <nugurejeil1@gmail.com>
2025-05-26 18:46:28 +0900

    0526 수업 내용 업로드(useCallback, useContext)

--- react/0523/react-usememo/src/ProductsList.jsx
+++ react/0523/react-usememo/src/ProductsList.jsx
@@ -1,9 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import './productsList.css';
 
 const ProductsList = () => {
 
     const [products, setProducts] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("all");
+    const [sortByRating, setSortByRating] = useState(false);
+
+    // 카테고리 필터함수
+    const filteredByCategory = useMemo(() => {
+
+        console.log('카테고리 변경');
+        if (selectedCategory === 'all') return products;
+
+        return products.filter(product => product.category === selectedCategory);
+
+    }, [products, selectedCategory]);
+
+
+    // 평점 정렬함수
+    const sortedProduct = useMemo(() => {
+
+        console.log('정렬 수행');
+        const productToSort = [...filteredByCategory];
+
+        if (sortByRating) {
+            return productToSort.sort((a, b) => b.rating - a.rating);
+        }
+
+        return productToSort
+    }, [filteredByCategory, sortByRating]);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -28,50 +55,32 @@ const ProductsList = () => {
         <div className="product-container">
             <h1>상품 목록</h1>
             <div className="product-controls">
-                <select name="category" className="product-select">
+                <select name="category" className="product-select" onChange={(e) => setSelectedCategory(e.target.value)}>
                     <option value="all">전체 카테고리</option>
-                    <option value="book">도서</option>
-                    <option value="food">식품</option>
-                    <option value="electronics">전자기기</option>
-                    <option value="cloth">의류</option>
+                    <option value="도서">도서</option>
+                    <option value="식품">식품</option>
+                    <option value="전자기기">전자기기</option>
+                    <option value="의류">의류</option>
                 </select>
                 <label className="rating-toggle">
-                    <input type="checkbox" />
+                    <input type="checkbox" checked={sortByRating} onChange={(e) => setSortByRating(e.target.checked)} />
                     평점순 정렬
                 </label>
             </div>
 
             <ul className="product-list">
-                <li className="product-item">
-                    <div className="product-info">
-                        <strong>자바스크립트 완벽가이드</strong>
-                        <span>도서</span>
-                    </div>
-                    <div className="product-value">
-                        <strong>45,000원</strong>
-                        <em>★ 4.9</em>
-                    </div>
-                </li>
-                <li className="product-item">
-                    <div className="product-info">
-                        <strong>자바스크립트 완벽가이드</strong>
-                        <span>도서</span>
-                    </div>
-                    <div className="product-value">
-                        <strong>45,000원</strong>
-                        <em>★ 4.9</em>
-                    </div>
-                </li>
-                <li className="product-item">
-                    <div className="product-info">
-                        <strong>자바스크립트 완벽가이드</strong>
-                        <span>도서</span>
-                    </div>
-                    <div className="product-value">
-                        <strong>45,000원</strong>
-                        <em>★ 4.9</em>
-                    </div>
-                </li>
+                {sortedProduct.map(product => {
+                    return <li key={product.id} className="product-item">
+                        <div className="product-info">
+                            <strong>{product.name}</strong>
+                            <span>{product.category}</span>
+                        </div>
+                        <div className="product-value">
+                            <strong>{product.price.toLocaleString('ko-kr')}원</strong>
+                            <em>★ {product.rating}</em>
+                        </div>
+                    </li>
+                })}
             </ul>
         </div>
     )
