adc882578c78eedf59dc8bf6befb81936f0d929f
nugurejeil <nugurejeil1@gmail.com>
2025-05-29 11:49:09 +0900

    0529 수업내용 업로드(쓰로틀, react clone)

--- react/0528/react-customhook/src/App.js
+++ react/0528/react-customhook/src/App.js
@@ -1,20 +1,33 @@
 import { useMouseLocation } from "./useMouseLocation";
 import { useScroll } from "./useScroll";
 import { useScrollObserver } from "./useScrollObserver";
-
+import { useState } from 'react';
 function App() {
 
   // const mouseLocation = useMouseLocation();
 
   const isBottom = useScrollObserver();
 
-  console.log(isBottom);
-
   return (
     <div className="App" style={{ width: 200, height: 1200, backgroundColor: 'teal' }}>
       hello react
+      <Test />
     </div>
+
   );
+}
+
+function Test() {
+  const [test, setTest] = useState(0);
+
+  const increment = () => {
+    setTest(test + 1);
+    setTest(test + 1);
+    setTest(test + 1);
+    setTest(test + 1);
+  }
+  console.log(test);
+  return <button onClick={increment}>test!</button>
 }
 
 export default App;
